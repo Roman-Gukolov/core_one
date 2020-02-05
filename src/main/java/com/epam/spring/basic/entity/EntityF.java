3612f3644fc2cf6c8639e3ecf4c959805f3ede70
Roman Gukolov <roma.gykolov@yandex.ru>
2020-02-05 17:11:15 +0300

    fix bean life cycle

--- src/main/java/com/epam/spring/basic/entity/EntityF.java
+++ src/main/java/com/epam/spring/basic/entity/EntityF.java
@@ -1,10 +1,10 @@
 package com.epam.spring.basic.entity;
 
 import org.apache.log4j.Logger;
-import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
-import org.springframework.beans.factory.config.BeanPostProcessor;
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 
 public class EntityF implements DisposableBean, InitializingBean {
     private static final Logger logger = Logger.getLogger(EntityF.class);
@@ -21,5 +21,15 @@ public class EntityF implements DisposableBean, InitializingBean {
     @Override
     public void afterPropertiesSet() throws Exception {
         logger.info("Initializing method of bean was invoked!");
+    }
+
+    @PostConstruct
+    public void postConstruct() {
+        logger.info("postConstruct method of bean was invoked!");
+    }
+
+    @PreDestroy
+    public void preDestroy() {
+        logger.info("preDestroy method of bean was invoked!");
     }
 }
